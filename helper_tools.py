4c25579f55a4a425f7798340e7cc31c35bdee171
Wouter <wouter-vrielink@hotmail.com>
2019-01-23 23:02:34 +0100

    statistic tests

--- helper_tools.py
+++ helper_tools.py
@@ -3,6 +3,7 @@ import os
 import itertools as it
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as sc
 
 
 def build_path(alg, bench_function, version, dims):
@@ -73,6 +74,11 @@ def get_data(path, file_list, column_name):
     return data
 
 
+def get_color(alg):
+    # Blue if PPA, orange if FWA
+    return '#1f77b4' if alg == PlantPropagation else '#ff7f0e'
+
+
 def plot_avg(alg, bench_function, version, dim, correction=0):
     path = build_path(alg, bench_function, version, dim)
 
@@ -83,12 +89,37 @@ def plot_avg(alg, bench_function, version, dim, correction=0):
     N = len(file_list) - 1
 
     all_best_y = np.matrix(list(it.zip_longest(*data, fillvalue=np.nan)))[:10000] - correction
-    mean_best_y = np.nanmean(all_best_y, axis=1)
+    median_best_y = np.median(all_best_y, axis=1)
 
     x = range(1, 10001)
 
-    plt.semilogy(x, mean_best_y, label=f'{alg.__name__} (N={N})')
-    plt.fill_between(x, np.percentile(all_best_y, 5, axis=1), np.percentile(all_best_y, 95, axis=1), alpha=0.2)
+    color = get_color(alg)
+
+    plt.semilogy(x, median_best_y, label=f'{alg.__name__}', color=color)
+    plt.fill_between(x, np.percentile(all_best_y, 0, axis=1), np.percentile(all_best_y, 25, axis=1), alpha=0.2, color=color, linewidth=0.0)
+    plt.fill_between(x, np.percentile(all_best_y, 25, axis=1), np.percentile(all_best_y, 75, axis=1), alpha=0.4, color=color, linewidth=0.0)
+    plt.fill_between(x, np.percentile(all_best_y, 75, axis=1), np.percentile(all_best_y, 100, axis=1), alpha=0.2, color=color, linewidth=0.0)
+
+
+def wilcoxon_test(alg, bench_function, bench_function_add, version="DEFAULT", dim="2"):
+    path = build_path(alg, bench_function, version, dim)
+    path_add = build_path(alg, bench_function_add, version, dim)
+
+    file_list = os.listdir(path)
+    file_list_add = os.listdir(path)
+
+    data = get_data(path, file_list, "curbest")
+    data_add = get_data(path_add, file_list_add, "curbest")
+
+    data = np.matrix(list(it.zip_longest(*data, fillvalue=np.nan)))[:10000]
+    data_add = np.matrix(list(it.zip_longest(*data_add, fillvalue=np.nan)))[:10000]
+
+    print(np.mean(data, axis=1) - np.mean(data, axis=1))
+
+    data = np.squeeze(np.mean(data, axis=1)).ravel().tolist()[0]
+    data_add = np.squeeze(np.mean(data_add, axis=1)).ravel().tolist()[0]
+
+    print(sc.mannwhitneyu(data, data_add))
 
 
 def plot_end_all_dims(alg, bench_function, version):
@@ -103,18 +134,18 @@ def plot_end_all_dims(alg, bench_function, version):
 
         data = get_data(path, file_list, "curbest")
 
-        N = len(file_list) - 1
+        # N = len(file_list) - 1
 
         all_best_y = list(it.zip_longest(*data, fillvalue=np.nan))[9999]
-        avg = np.nanmean(all_best_y)
+        avg = np.median(all_best_y)
 
         avgs.append(avg)
 
         # We need the absolute errors
-        err_lo.append(avg - np.percentile(all_best_y, 5))
-        err_hi.append(np.percentile(all_best_y, 95) - avg)
+        err_lo.append(avg - np.percentile(all_best_y, 0))
+        err_hi.append(np.percentile(all_best_y, 100) - avg)
 
-    plt.errorbar(range(2, 101), avgs, yerr=[err_lo, err_hi], fmt='o', label=f'{alg.__name__} (N={N})', capsize=2)
+    plt.errorbar(range(2, 101), avgs, yerr=[err_lo, err_hi], fmt='o', label=f'{alg.__name__}', capsize=2, color=get_color(alg))
 
 
 def plot_times(version="DEFAULT"):
@@ -142,11 +173,13 @@ def plot_times(version="DEFAULT"):
             avg.append(np.mean(temp))
             std.append(np.std(temp))
 
-        plt.errorbar(range(2, 101), avg, yerr=[std, std], fmt='o', label=f'{alg.__name__}', capsize=2)
+        plt.errorbar(range(2, 101), avg, yerr=[std, std], fmt='o', label=f'{alg.__name__}', capsize=2, color=get_color(alg))
 
+    ax = plt.gca()
+    ax.set_xlim((2, 101))
     plt.xlabel('Dimension')
     plt.ylabel('Time (in seconds)')
-    plt.title('Time to complete 10000 evaluations over all benchmarks')
+    plt.title('Time to complete 10000 evaluations')
     plt.legend()
 
     plt.savefig(f'{fpath}/{filename}', bbox_inches='tight')
@@ -169,18 +202,16 @@ def plot_end_all_shifts(alg, bench_function, shifts, version, correction=0):
 
         data = get_data(path, file_list, "curbest")
 
-        N = len(file_list) - 1
-
         all_best_y = np.matrix(list(it.zip_longest(*data, fillvalue=np.nan))[9999]) - correction
-        avg = np.nanmean(all_best_y)
+        avg = np.percentile(all_best_y, 50)
 
         avgs.append(avg)
 
         # We need the absolute errors
-        err_lo.append(avg - np.percentile(all_best_y, 5))
-        err_hi.append(np.percentile(all_best_y, 95) - avg)
+        err_lo.append(avg - np.percentile(all_best_y, 0))
+        err_hi.append(np.percentile(all_best_y, 100) - avg)
 
-    plt.errorbar(shifts, avgs, yerr=[err_lo, err_hi], fmt='o', label=f'{alg.__name__} (N={N})', capsize=2)
+    plt.errorbar(shifts, avgs, yerr=[err_lo, err_hi], fmt='o', label=f'{alg.__name__}', capsize=2, color=get_color(alg))
 
 
 def get_plot_path(bench_function):
@@ -189,7 +220,7 @@ def get_plot_path(bench_function):
 
 def plot_versus(bench_function, dims, version="DEFAULT", correction=0):
     path = get_plot_path(bench_function)
-    filename = f'{dims}d.png'
+    filename = f'{bench_function.__name__}_{dims}d.png'
 
     check_folder(path)
 
@@ -200,8 +231,8 @@ def plot_versus(bench_function, dims, version="DEFAULT", correction=0):
     plot_avg(PlantPropagation, bench_function, version, dims, correction=correction)
 
     plt.xlabel('Evaluation')
-    plt.ylabel('Benchmark score')
-    plt.title('Mean of bests')
+    plt.ylabel('Benchmark score (normalised)')
+    plt.title(f'Benchmark results (N=10, {bench_function.__name__})')
     plt.legend()
 
     plt.savefig(f'{path}/{filename}', bbox_inches='tight')
@@ -209,7 +240,7 @@ def plot_versus(bench_function, dims, version="DEFAULT", correction=0):
 
 def plot_versus_dims(bench_function, version="DEFAULT"):
     path = get_plot_path(bench_function)
-    filename = f'all_dims.png'
+    filename = f'{bench_function.__name__}_all_dims.png'
 
     check_folder(path)
 
@@ -222,9 +253,10 @@ def plot_versus_dims(bench_function, version="DEFAULT"):
     ax = plt.gca()
     ax.set_yscale("log", nonposy='clip')
 
+    ax.set_xlim((2, 101))
     plt.xlabel('Dimension')
-    plt.ylabel('Benchmark score')
-    plt.title('Mean of results after 10000 evaluations')
+    plt.ylabel('Benchmark score (normalised)')
+    plt.title(f'Results after 10000 evaluations (N=10, {bench_function.__name__})')
     plt.legend()
 
     plt.savefig(f'{path}/{filename}', bbox_inches='tight')
@@ -232,7 +264,7 @@ def plot_versus_dims(bench_function, version="DEFAULT"):
 
 def plot_versus_shift(bench_function, shifts, version="DEFAULT", correction=0):
     path = get_plot_path(bench_function)
-    filename = f'shifts.png'
+    filename = f'{bench_function.__name__}_shifts.png'
 
     check_folder(path)
 
@@ -247,8 +279,8 @@ def plot_versus_shift(bench_function, shifts, version="DEFAULT", correction=0):
     ax.set_xscale('symlog', linthreshx=0.1)
 
     plt.xlabel('Shift')
-    plt.ylabel('Benchmark score')
-    plt.title('Mean of results after 10000 evaluations')
+    plt.ylabel('Benchmark score (normalized)')
+    plt.title(f'Results after 10000 evaluations (N=10, {bench_function.__name__})')
     plt.legend()
 
     plt.savefig(f'{path}/{filename}', bbox_inches='tight')
@@ -256,7 +288,7 @@ def plot_versus_shift(bench_function, shifts, version="DEFAULT", correction=0):
 
 def plot_compare_center_single(bench_function, bench_function_center, version="DEFAULT", correction=0):
     path = get_plot_path(bench_function)
-    filename = f'centered.png'
+    filename = f'{bench_function.__name__}_centered.png'
 
     check_folder(path)
 
@@ -273,8 +305,8 @@ def plot_compare_center_single(bench_function, bench_function_center, version="D
     ax.set_yscale('symlog', nonposy='clip', linthreshy=0.0000001)
 
     plt.xlabel('Shift')
-    plt.ylabel('Benchmark score')
-    plt.title('Mean of results after 10000 evaluations')
+    plt.ylabel('Benchmark score (normalized)')
+    plt.title(f'Results after 10000 evaluations (N=10, {bench_function.__name__})')
     plt.legend()
 
     plt.savefig(f'{path}/{filename}', bbox_inches='tight')
@@ -285,23 +317,30 @@ if __name__ == '__main__':
     from fireworks import Fireworks
     import benchmarks
 
-    plot_times()
+    # plot_times()
 
     # print("Plotting 2d benchmarks...")
 
-    # Comparison between non-centered function and the centered version
+    # # Comparison between non-centered function and the centered version
     # for bench_function in benchmarks.two_dim_non_centered_bench_functions():
     #     bounds, correction = benchmarks.two_dim_bench_functions()[bench_function]
     #     bench_function_center, _ = benchmarks.apply_add(bench_function, bounds, name='_center')
     #
     #     plot_compare_center_single(bench_function, bench_function_center, correction=correction)
-    #
-    # # Comparison between fwa and ppa, and comparison for different shift sizes
+
+    # # Comparison between fwa and ppa, centered and non-centered, and comparison for different shift sizes
     # for bench_function, (domain, correction) in benchmarks.two_dim_bench_functions().items():
     #     plot_versus(bench_function, 2, correction=correction)
     #
-    #     plot_versus_shift(bench_function, (0, 0.1, 1, 10, 100, 1000), correction=correction)
+        # bench_function_center, _ = benchmarks.apply_add(bench_function, domain, name='_center')
+
+        # for alg in (Fireworks, PlantPropagation):
+        #     wilcoxon_test(alg, bench_function, bench_function_center)
     #
+    #     plot_versus(bench_function_center, 2, correction=correction)
+    #
+    #     plot_versus_shift(bench_function, (0, 0.1, 1, 10, 100, 1000), correction=correction)
+    # #
     # # Comparisons between fwa and ppa for both unshifted and shifted benchmarks per dimension
     # for dims in range(2, 101):
     #     print(f'Plotting Nd benchmarks {dims}d/100d...')
@@ -315,11 +354,12 @@ if __name__ == '__main__':
     #         plot_versus(bench_function_add, dims)
     #
     # print("Plotting Nd benchmark commparisons...")
-    #
-    # # Comparisons over all dimensions for shifted and unshifted benchmarks
+
+    # Comparisons over all dimensions for shifted and unshifted benchmarks
     # for bench_function, domain in benchmarks.n_dim_bench_functions().items():
     #     plot_versus_dims(bench_function)
     #
+    #     print(f'{bench_function.__name__} done')
     #     domain = [domain for _ in range(100)]
     #     bench_function_add, domain_add = benchmarks.apply_add(bench_function, domain)
     #
@@ -328,3 +368,5 @@ if __name__ == '__main__':
     # TODO: Comparison of N-D shifted vs unshifted?
     # - Use results at 10k evals of all dims
     # - Seperate FWA and PPA so we only have 2 lines
+
+    # TODO N=10 dynamisch maken, rename avg naar median
